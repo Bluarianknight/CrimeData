b774d841fb81b9595518b243f1f61fb8867f6bfa
Bluarianknight <59690656+Bluarianknight@users.noreply.github.com>
2023-11-17 13:37:20 -0500

    Commented and Cleaned Up Beta
    Currently cleaning up the files - it now properly displays an application with the pandastable table on display. Current work needs to create a GUI to handle sorting it properly.

--- CrimeData/CrimeData.py
+++ CrimeData/CrimeData.py
@@ -1,48 +1,53 @@
 from email.policy import default
-from CrimeDict import *
-from pandas import DataFrame
+from CrimeDict import * # CrimeDict is used for managing the data in the back - currently not used yet proper as pandastable handles a lot of it.
+from pandas import DataFrame # If it wasn't imported specifically, it wouldn't work. 
 from pandas import *
-from tkinter import filedialog, messagebox, ttk
+from tkinter import filedialog, messagebox, ttk # Same reason as above - certain commands require it to load specifically.
 from tkinter import *
 from pandastable import Table, TableModel, config
 
 
+# Creates the root application window, setting it to 1280 to 720.
 root = Tk()
+root.geometry("1280x720")
 
-
-
+# Creates the menu bar at the top of the application.
 menu = Menu(root)
 item = Menu(menu)
 
 root.config(menu=menu)
 
 
-
+# Creates two frames - one to contain the pandastable table, and the other for either rich text or button prompts - unsure yet.
 frame = Frame(root)
-frame.pack(fill=BOTH,expand=1)
+frametwo = Frame(root)
+frame.pack(fill=BOTH,side="right",expand=1)
+frametwo.pack(fill=BOTH,side="left",expand=1)
 tabledata = TableModel.getSampleData()
 table = pt = Table(frame, dataframe=tabledata, showtoolbar=False, showstatusbar=True)
 
-#tree = ttk.Treeview(frame)
-#tree.place(relheight=1, relwidth=0.5)
-#treescrollx = Scrollbar(frame, orient="vertical", command=tree.xview_scroll)
-#treescrolly = Scrollbar(frame, orient="horizontal", command=tree.yview_scroll)
-#tree.configure(xscrollcommand=treescrollx, yscrollcommand=treescrolly)
-#treescrollx.pack(side="bottom", fill="x")
-#treescrolly.pack(side="right", fill="y")
 
 
+# Handles the file dialog to obtain file location, before sending it to loadfile(). Handles error exceptions.
+# Referenced https://gist.github.com/RamonWill/0686bd8c793e2e755761a8f20a42c762#file-tkinterexcel-py-L71 for this, alongside
+# https://www.pythontutorial.net/tkinter/tkinter-open-file-dialog/ helped as well to figure out some issued I had. 
 def findFile():
     
-    fileoptions = (("xlsx files", "*.xlsx"),("json files", "*.json"),("csv files,", "*.csv"),("All Files", "*.*"))
+    fileoptions = (("csv files,", "*.csv"),("xlsx files", "*.xlsx"),("json files", "*.json"),("All Files", "*.*"))
     filelocation = filedialog.askopenfilename(initialdir="/",
                                           title="Select A File",
                                           filetype=fileoptions)
-    
-    loadFile(filelocation)
-
+    try:
+        loadFile(filelocation)
+    except ValueError:
+        messagebox.showerror("Incorrect File Type!", "You selected an invalid file type.")
+        return None
+    except FileNotFoundError:
+        messagebox.showerror("File Not Found", "The selected file could not be found.")
+        return None
     return None
 
+# Uses the dataf variable as a database to load the file based on it's type, before updating the table with the dataframe. 
 def loadFile(filelocation):
     dataf = ''
     filepath = filelocation
@@ -53,33 +58,25 @@ def loadFile(filelocation):
     else:
          dataf = pd.read_csv(filepath)
     Crime.csvData = dataf
-    table = pt = Table(frame, dataframe=tabledata, showtoolbar=False, showstatusbar=True)
-    tabledata = dataf
+    table.updateModel(TableModel(dataf))
     table.update()
     table.redraw()    
     return None
         
     
-
+# Adds the findfile function to the Load File option. 
 item.add_command(label='Load File', command= findFile)
 menu.add_cascade(label='Menu', menu=item)
 
-# Creates the application window with tkinter, root being the application.
-
-
-# Creates a menu bar with 'File as the menu cascade, and a single command to load a file.
-
-
-openfile = LabelFrame(root, text='Open File')
-openfile.place(height=100, width=400, rely=0.65, relx=0)
-
-label_file = Label(openfile, text="No File Selected")
-label_file.place(rely=0, relx=0)
+#Shows the table, required. 
 table.show()
 
-
+# Redundant code - may be used later, so keeping it. 
 # pt.show
 # pt.importCSV('C:\\CO-ST\\CrimeData\\CrimeData\\NYPD.csv')
+
+# Everything below this is redundant for a command prompt handling of the data - being kept for testing purposed, 
+# and to eventually integrate the code into using the CrimeDict class properly. 
 
 print('Crime Data Report')
 print('This is a temporary measure.')
@@ -111,3 +108,5 @@ while menu != 'q':
         print('Incorrect. Please try again.')
         menu = input()
         
+# Concurrently the biggest issue is organization with the code - I plan on creating an application class to handle the application variables, and to clean
+# the code properly. 
